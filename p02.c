1208f479d17a9348c8675827963190e2d969f73a
pablolobat0 <plobatorey@gmail.com>
2025-02-16 16:58:33 +0100

    feat: añadida medición de tiempos de p02 y p03

--- p02.c
+++ p02.c
@@ -1,52 +1,45 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <math.h>
 
 #define TAM 83886080
 /**
  * @brief Template for Labs
- * 
+ *
  * PAE [G4011452] Labs
- * Last update: 
- * Issue date:  30/01/2022
- * 
- * Student name: Pablo Lobato Rey
+ * Last update:
+ * Issue date:  07/02/2025
+ *
+ * Student name: Pablo Lobato Rey y David Carpintero Diaz
  *
  */
 
 // General utilities
-void init_array(double *array, int tam) {
+void init_array(double *array, int tam, int alpha) {
     for (int i = 0; i < tam; i++) {
-         array[i] = i;
+        array[i] = i * alpha;
     }
 }
 
-
-void mult_array(double *array, int tam, int alpha) {
-    for (int i = 0; i < tam; i++) {
-         array[i] *= alpha;
-    }
-}
-
-// Custon utilities (in case of need) 
+// Custon utilities (in case of need)
 
 // Implement the exercise in a function here
-void euclidian_distance(double *a, double *b, int tam) {
-    float result;
+float euclidian_distance(double *a, double *b, int tam) {
+    float result = 0;
     for (int i = 0; i < tam; i++) {
-         result += ((a[i] - b[i]) * (a[i] - b[i]));
+        result += ((a[i] - b[i]) * (a[i] - b[i]));
     }
 
-    result = sqrtf(result);
+    return sqrtf(result);
 }
-
 
 // Main program
 int main(int argc, char *argv[]) {
     double *a, *b;
     struct timeval start, finish;
     double time;
+    float distancia = 0;
     int N;
 
     if (argc > 2) {
@@ -55,24 +48,40 @@ int main(int argc, char *argv[]) {
     }
 
     N = argc == 2 ? atoi(argv[1]) : TAM;
-
-    a = (double*) malloc(sizeof(double) * N);
-    b = (double*) malloc(sizeof(double) * N);
-
-    init_array(a, N);
-    init_array(b, N);
-    mult_array(b, N, 2);
     // start timer
     gettimeofday(&start, NULL);
-    
-    // call the function
-    euclidian_distance(a, b, N);
+
+    a = (double *)malloc(sizeof(double) * N);
+    b = (double *)malloc(sizeof(double) * N);
+
+    if (a == NULL || b == NULL) {
+        printf("Error: No se pudo asignar memoria.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    init_array(a, N, 2);
+    init_array(b, N, 1);
 
     // stop timer
     gettimeofday(&finish, NULL);
-    time = (finish.tv_sec - start.tv_sec + (finish.tv_usec - start.tv_usec) / 1.e6);
+    time = (finish.tv_sec - start.tv_sec +
+            (finish.tv_usec - start.tv_usec) / 1.e6);
 
-    printf("%lf\n", time);
+    printf("Reserva de memoria: %.10lf\n", time);
+
+    // start timer
+    gettimeofday(&start, NULL);
+
+    // call the function
+    distancia = euclidian_distance(a, b, N);
+
+    // stop timer
+    gettimeofday(&finish, NULL);
+    time = (finish.tv_sec - start.tv_sec +
+            (finish.tv_usec - start.tv_usec) / 1.e6);
+
+    printf("Ejecucion: %.10lf\n", time);
+    // printf("Distancia: %f\n", distancia);
 
     free(a);
     free(b);
--- p03.c
+++ p03.c
@@ -1,41 +1,83 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
 #include "./Lab1_material/pgmio.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
 #define SIZE 256
+/**
+ * @brief Template for Labs
+ *
+ * PAE [G4011452] Labs
+ * Last update:
+ * Issue date:  30/01/2022
+ *
+ * Student name: Pablo Lobato Rey y David Carpintero Diaz
+ *
+ */
 
+// General utilities
+
+// Custon utilities (in case of need)
+
+// Implement the exercise in a function here
+void histogram(unsigned char *image, int height, int width, int *count) {
+    for (int i = 0; i < height * width; i++) {
+        count[image[i]] += 1;
+    }
+}
+
+// Main program
 int main(int argc, char *argv[]) {
+    int width, height;
+    unsigned char *image;
+    struct timeval start, finish;
+    double time;
+    int *count;
     // check for arguments
     if (argc < 2) {
-    	printf("Use %s file.pgm\n", argv[0]);
+        printf("Use %s file.pgm\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    // start timer
+    gettimeofday(&start, NULL);
+
+    count = (int *)calloc(SIZE, sizeof(int));
+
+    if (count == NULL) {
+        printf("Error: No se pudo asignar memoria.\n");
         exit(EXIT_FAILURE);
     }
 
-    int *count;
-
-    count = malloc(sizeof(int) * SIZE);
-
-    for (int i = 0; i < SIZE; i++) {
-        count[i] = 0;
-    }
-
-    // image width x height
-    int width, height;
-
     // Load pgm image
-    unsigned char* xu8 = loadPGMu8(argv[1], &width, &height);
+    image = loadPGMu8(argv[1], &width, &height);
 
-    for (int i = 0; i < height * width; i++) {
-        count[xu8[i]] += 1;
-    }
+    // stop timer
+    gettimeofday(&finish, NULL);
+    time = (finish.tv_sec - start.tv_sec +
+            (finish.tv_usec - start.tv_usec) / 1.e6);
 
-    for (int i = 0; i<SIZE; i++) {
-        printf("%d ", count[i]); 
-    }
-    printf("\n");
+    printf("Reserva de memoria: %.10lf\n", time);
 
-    free(xu8);
+    // start timer
+    gettimeofday(&start, NULL);
+
+    // call the function
+    histogram(image, height, width, count);
+
+    // stop timer
+    gettimeofday(&finish, NULL);
+    time = (finish.tv_sec - start.tv_sec +
+            (finish.tv_usec - start.tv_usec) / 1.e6);
+
+    printf("Ejecucion: %.10lf\n", time);
+
+    // for (int i = 0; i < SIZE; i++) {
+    //     printf("%d ", count[i]);
+    // }
+    // printf("\n");
+
+    free(image);
     free(count);
 
     return EXIT_SUCCESS;
