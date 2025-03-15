cf40c3779213c5e1586b11ab444f424eff098854
pablolobat0 <plobatorey@gmail.com>
2025-03-15 17:10:31 +0100

    códigos corregidos

--- P2/global_histograma.cu
+++ P2/global_histograma.cu
@@ -1,6 +1,6 @@
 // General utilities
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 // Math functions
 #include <math.h>
 
@@ -8,60 +8,57 @@
 #include "../pgmio.h"
 
 #define SIZE 256
-#define BLOCK_SIZE 1024
+#define BLOCK_SIZE 256
 
-__global__ void histograma(unsigned char * d_xu8, int* v_hist, int dim){
-    int i = threadIdx.x + blockDim.x * blockIdx.x;
-    //hay que poner un mutex, si no va a haber carreras criticas
-    if(i<dim)
+__global__ void histograma(unsigned char *d_xu8, int *v_hist, int dim) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    // hay que poner un mutex, si no va a haber carreras criticas
+    for (int i = idx; i < dim; i += stride) {
         atomicAdd(&v_hist[d_xu8[i]], 1);
+    }
 }
 
+int main(int argc, char *argv[]) {
+    int blockDim, blockCount;
+    int width, height;
+    int h_hist[SIZE] = {0};
+    unsigned char *h_xu8, *v_xu8;
+    int *valores_hist;
 
-int main(int argc, char *argv[])
-{ 
     // check for arguments
     if (argc < 2) {
-    	printf("Use %s file.pgm\n", argv[0]);
+        printf("Use %s file.pgm\n", argv[0]);
         exit(-1);
     }
 
-    // image width x height
-    int w, h;
-
     // Load pgm image
-    unsigned char* h_xu8 = loadPGMu8(argv[1], &w, &h);
+    h_xu8 = loadPGMu8(argv[1], &width, &height);
 
-    int h_hist[SIZE]= {0};
+    // reservamos memoria para las variables en memoria GLOBAL
+    cudaMalloc(&v_xu8, width * height * sizeof(unsigned char));
+    cudaMalloc(&valores_hist, SIZE * sizeof(int));
 
-    unsigned char* v_xu8;
-    int* v_hist;
+    // inicalizamos variables en device
+    cudaMemset(valores_hist, 0, SIZE * sizeof(int));
+    cudaMemcpy(v_xu8, h_xu8, width * height * sizeof(unsigned char),
+               cudaMemcpyHostToDevice);
 
-    //reservamos memoria para las variables en memoria GLOBAL
-    cudaMalloc(&v_xu8,w*h*sizeof(unsigned char));
-    cudaMalloc(&v_hist, SIZE*sizeof(int));
-
-    //inicalizamos variables en device
-    cudaMemset(v_hist,0,SIZE*sizeof(int));
-    cudaMemcpy(v_xu8,h_xu8,w*h*sizeof(unsigned char),cudaMemcpyHostToDevice);
-  
-    int blockDim,blockCount;
     blockDim = BLOCK_SIZE;
-    blockCount = (w * h + blockDim - 1) / blockDim;
-    histograma<<<blockCount,blockDim>>>(v_xu8, v_hist, w*h);
-     
+    blockCount = (width * height + blockDim - 1) / blockDim;
+    histograma<<<blockCount, blockDim>>>(v_xu8, valores_hist, width * height);
 
-    cudaMemcpy(h_hist,v_hist,SIZE*sizeof(int),cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_hist, valores_hist, SIZE * sizeof(int),
+               cudaMemcpyDeviceToHost);
 
-    for (int i = 0; i<SIZE; i++) {
-        printf("%i ",h_hist[i]); 
+    for (int i = 0; i < SIZE; i++) {
+        printf("%i ", h_hist[i]);
     }
     printf("\n");
+
     free(h_xu8);
     cudaFree(v_xu8);
-    cudaFree(v_hist);
+    cudaFree(valores_hist);
 
-    
     return 0;
-
 }
--- P2/shared_histograma.cu
+++ P2/shared_histograma.cu
@@ -1,89 +1,89 @@
 // General utilities
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 // Math functions
 #include <math.h>
 
 // The file loadPGM.h will be used for defining load and export functions
-#include "pgmio.h"
+#include "../pgmio.h"
 
 #define SIZE 256
-#define BLOCK_SIZE 1024
+#define BLOCK_SIZE 256
 
-__global__ void histograma(unsigned char * d_xu8, int* v_hist, int dim){
+__global__ void histograma(unsigned char *d_xu8, int *v_hist, int dim) {
     __shared__ int histo_s[SIZE];
 
     int tid = threadIdx.x;
-    if(tid<SIZE)histo_s[tid]=0;
-    __syncthreads();
-    int i = threadIdx.x + blockIdx.x*blockDim.x;
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
 
-    while (i<dim){ //loop por si el numero de hilos es menor que numero de pixeles
-        atomicAdd(&histo_s[d_xu8[i]],1); //usamos atomic para evitar carreras criticas
-        i+= blockIdx.x*blockDim.x;
+    // Inicilizacion de la memoria compartida
+    for (int i = tid; i < SIZE; i += blockDim.x) {
+        histo_s[i] = 0;
     }
+
+    __syncthreads();
+
+    // loop por si el numero de hilos es menor que numero de pixeles
+    for (int i = idx; i < dim; i += stride) {
+        // usamos atomic para evitar carreras criticas
+        atomicAdd(&histo_s[d_xu8[i]], 1);
+    }
+
     __syncthreads();
 
     // Un solo hilo por nivel de intensidad actualiza la memoria global
-    if (tid < SIZE) {
-        atomicAdd(&v_hist[tid], histo_s[tid]);
+    for (int i = tid; i < SIZE; i += blockDim.x) {
+        atomicAdd(&v_hist[i], histo_s[i]);
     }
 }
 
+int main(int argc, char *argv[]) {
+    unsigned char *h_xu8, *v_xu8;
+    int width, height;
+    int h_hist[SIZE] = {0};
+    int *v_hist;
+    int blockDim, blockCount;
 
-int main(int argc, char *argv[])
-{ 
     // check for arguments
     if (argc < 2) {
-    	printf("Use %s file.pgm\n", argv[0]);
+        printf("Use %s file.pgm\n", argv[0]);
         exit(-1);
     }
 
-    // image width x height
-    int w, h;
-
     // Load pgm image
-    unsigned char* h_xu8 = loadPGMu8(argv[1], &w, &h);
+    h_xu8 = loadPGMu8(argv[1], &width, &height);
 
-    int h_hist[SIZE]= {0};
+    // reservamos memoria para las variables en memoria GLOBAL
+    cudaMalloc(&v_xu8, width * height * sizeof(unsigned char));
+    cudaMalloc(&v_hist, SIZE * sizeof(int));
 
-    unsigned char* v_xu8;
-    int* v_hist;
+    // inicalizamos variables en device
+    cudaMemset(v_hist, 0, SIZE * sizeof(int));
+    cudaMemcpy(v_xu8, h_xu8, width * height * sizeof(unsigned char),
+               cudaMemcpyHostToDevice);
 
-    //reservamos memoria para las variables en memoria GLOBAL
-    cudaMalloc(&v_xu8,w*h*sizeof(unsigned char));
-    cudaMalloc(&v_hist, SIZE*sizeof(int));
-
-    //inicalizamos variables en device
-    cudaMemset(v_hist,0,SIZE*sizeof(int));
-    cudaMemcpy(v_xu8,h_xu8,w*h*sizeof(unsigned char),cudaMemcpyHostToDevice);
-  
-    int blockDim,blockCount;
     blockDim = BLOCK_SIZE;
-    blockCount = (w * h + blockDim - 1) / blockDim;
-    histograma<<<blockCount,blockDim>>>(v_xu8, v_hist, w*h);
+    blockCount = (width * height + blockDim - 1) / blockDim;
+    histograma<<<blockCount, blockDim>>>(v_xu8, v_hist, width * height);
     cudaDeviceSynchronize();
-     
-    cudaMemcpy(h_hist,v_hist,SIZE*sizeof(int),cudaMemcpyDeviceToHost);
-    
+
+    cudaMemcpy(h_hist, v_hist, SIZE * sizeof(int), cudaMemcpyDeviceToHost);
+
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess) {
         printf("CUDA error: %s\n", cudaGetErrorString(err));
     }
 
     cudaDeviceSynchronize();
-    for (int i = 0; i<SIZE; i++) {
-            printf("%i ",h_hist[i]); 
-        }
-        printf("\n");
-
-
+    for (int i = 0; i < SIZE; i++) {
+        printf("%i ", h_hist[i]);
+    }
+    printf("\n");
 
     free(h_xu8);
     cudaFree(v_xu8);
     cudaFree(v_hist);
 
-    
     return 0;
-
 }
